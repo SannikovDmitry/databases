494ae79fee945d8464e8974bf4a6595cf733f1e8
Dmitry Sannikov <sdk96@mail.ru>
2017-01-11 06:39:09 -0800

    Update

--- src/main/java/ru/mail/park/Configurations/MysqlConnectionConfig.java
+++ src/main/java/ru/mail/park/Configurations/MysqlConnectionConfig.java
@@ -16,7 +16,7 @@ import javax.sql.DataSource;
 public class MysqlConnectionConfig {
 
     public static final String DRIVER = "com.mysql.jdbc.Driver";
-    public static final String URL = "jdbc:mysql://localhost:3306/Database?autoreconnect=true&useUnicode=yes&useSSL=false&characterEncoding=UTF-8";
+    public static final String URL = "jdbc:mysql://localhost/Database?autoreconnect=true&useUnicode=yes&useSSL=false&characterEncoding=UTF-8";
     public static final String USER = "root";
     public static final String PASSWORD = "626836dima";
     public static final int MAX_ACTIVE = 5;
