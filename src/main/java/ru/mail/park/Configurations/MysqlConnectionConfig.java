3ed31472fe6c34c9fe61144a15ed0fb29bb62216
Dmitry Sannikov <sdk96@mail.ru>
2017-01-11 05:06:20 -0800

    Update

--- src/main/java/ru/mail/park/Configurations/MysqlConnectionConfig.java
+++ src/main/java/ru/mail/park/Configurations/MysqlConnectionConfig.java
@@ -6,6 +6,7 @@ import org.apache.commons.dbcp.PoolableConnectionFactory;
 import org.apache.commons.dbcp.PoolingDataSource;
 import org.apache.commons.pool.impl.GenericObjectPool;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
@@ -13,12 +14,14 @@ import javax.sql.DataSource;
 @Configuration
 @EnableAutoConfiguration
 public class MysqlConnectionConfig {
+
     public static final String DRIVER = "com.mysql.jdbc.Driver";
     public static final String URL = "jdbc:mysql://localhost:3306/Database?autoreconnect=true&useUnicode=yes&useSSL=false&characterEncoding=UTF-8";
     public static final String USER = "root";
     public static final String PASSWORD = "626836";
     public static final int MAX_ACTIVE = 5;
 
+    @Bean
     public DataSource createSource() throws Exception
     {
         Class.forName(DRIVER).newInstance();
